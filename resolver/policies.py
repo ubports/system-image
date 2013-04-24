fd550c0758848f2d425d86ee5e14dd8798470509
Barry Warsaw <barry@python.org>
2013-04-24 18:25:31 -0400

    Candidate resolution policies.

---
+++ resolver/policies.py
@@ -0,0 +1,55 @@
+# Copyright (C) 2013 Canonical Ltd.
+# Author: Barry Warsaw <barry@ubuntu.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Upgrade policy decisions.
+
+Choose which upgrade path to use based on the available candidates.
+"""
+
+__all__ = [
+    'ByDownloadSize',
+    'Policy',
+    ]
+
+
+class Policy:
+    """Abstract base class providing an API for candidate selection."""
+
+    def choose(self, candidates):
+        """Choose an upgrade path from the set of candidate paths.
+
+        Subclasses are expected to override this method.
+
+        :param candidates: A list of lists of image records needed to upgrade
+            the device from the current version to the latest version, sorted
+            in order from oldest verson to newest.
+        :type candidates: list of lists
+        :return: The upgrade path matching the class's defined policy.
+        :rtype: list
+        """
+        raise NotImplementedError
+
+
+class ByDownloadSize(Policy):
+    def choose(self, candidates):
+        sizes = []
+        for path in candidates:
+            size = sum(image.size for image in path)
+            sizes.append((size, path))
+        # The first item of the sorted sizes will be the smallest one in terms
+        # of total download size.  The second item of that list will be the
+        # candidate paths.
+        smallest = sorted(sizes)[0][1]
+        return smallest
---
+++ resolver/tests/test_policies.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2013 Canonical Ltd.
+# Author: Barry Warsaw <barry@ubuntu.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__all__ = [
+    'TestPolicies',
+    ]
+
+
+import unittest
+
+from resolver.candidates import get_candidates
+from resolver.policies import ByDownloadSize
+from resolver.tests.helpers import get_index
+
+
+class TestPolicies(unittest.TestCase):
+    def setUp(self):
+        self.index = get_index('stable_nexus7_index_02.json')
+
+    def test_by_download_size(self):
+        # This policy optimizes for minimal download size.
+        ubuntu, android = get_candidates(self.index, '20130200', '20130200')
+        # Going straight to the latest full is a smaller download.
+        smallest = ByDownloadSize().choose(ubuntu)
+        self.assertEqual(len(smallest), 1)
+        self.assertEqual(smallest[0].checksum,
+                         '5a37ba30664cde4ab245e337c12d16f8ad892278')
+        # Because of the sample data, it's actually less to download (by 1
+        # byte :) the Android monthly + delta.
+        smallest = ByDownloadSize().choose(android)
+        self.assertEqual(len(smallest), 2)
+        monthly, delta = smallest
+        self.assertEqual(monthly.checksum,
+                         'd513dc5e4ed887d8c56e138386f68c8e33f93002')
+        self.assertEqual(monthly.version, '20130300')
+        self.assertEqual(delta.checksum,
+                         'da39a3ee5e6b4b0d3255bfef95601890afd80709')
+        self.assertEqual(delta.version, '20130301')
