0ba0a7120d0a227c6d980e2ac0f95010b2195628
Barry Warsaw <barry@python.org>
2015-02-02 15:27:50 -0500

    * Refactor downloading lock. * Fix tox.ini file so commands aren't run too often.

--- systemimage/testing/dbus.py
+++ systemimage/testing/dbus.py
@@ -78,11 +78,6 @@ class _LiveTestableService(Service):
         except RuntimeError:
             # Lock is already released.
             pass
-        try:
-            self._downloading.release()
-        except RuntimeError:
-            # Lock is already released.
-            pass
         self._update = None
         self._rebootable = False
         self._failure_count = 0
