5b3cf7eebbcb0645eb74aa2148b1d70ef3d56bce
Florian Leeber <flori@bin.org.in>
2020-02-09 20:33:50 +0100

    Lets try a null check

--- systemimage/device.py
+++ systemimage/device.py
@@ -43,7 +43,7 @@ class SystemProperty(BaseDevice):
                 'getprop ro.product.device'.split(), universal_newlines=True)
         except:
             pass
-        if stdout == '':
+        if stdout and stdout == '':
             # Try to use device-info instead
             try:
                 stdout = check_output(
